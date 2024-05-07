245d0cb61dadc406e97e7f0275facaeccb46bbdb
kolliDayakar77kolli <Kollidayakar77@gmail.com>
2024-05-07 15:15:52 +0530

    Added server js & chakra UI & BrowserRoute setup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
+
+app.get('/',(req,res) => {
+    res.send('API is Running Successfully...');
+})
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT,console.log("Server Started Running On PORT: 5000"));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
+import { Button } from '@chakra-ui/react'
+import Homepage from './pages/Homepage';
+import { Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-     hi
+     <Route path="/" component={Homepage} />
+     <Button colorScheme='blue'>Button</Button>
     </div>
   );
 }
