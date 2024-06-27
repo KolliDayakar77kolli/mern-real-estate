78a32b2610b6f25048c74f1a002578edc095f62a
kolliDayakar77kolli <Kollidayakar77@gmail.com>
2024-06-27 15:26:01 +0530

    ,,,

--- frontend/src/components/Miscellaneous/Unique.js
+++ frontend/src/components/Miscellaneous/Unique.js
@@ -10,8 +10,7 @@ import { Link } from "react-router-dom";
 
 const Unique = () => {
   const flexDirection = useBreakpointValue({ base: "column", md: "row" });
-  const minHeight = useBreakpointValue({ base: "400px", md: "500px" });
-  const maxHeight = useBreakpointValue({ base: "400px", md: "500px" });
+  const cardHeight = useBreakpointValue({ base: "auto", md: "300px" });
 
   return (
     <Flex
@@ -22,8 +21,8 @@ const Unique = () => {
       bg="white"
       align="center"
       overflow="hidden"
-      minH={minHeight}
-      maxH={maxHeight}
+      height={cardHeight}
+      width="100%"
     >
       {/* Left side with image */}
       <Flex
@@ -31,14 +30,13 @@ const Unique = () => {
         justify="center"
         align="center"
         p={1}
-        minH={minHeight}
-        maxH={maxHeight}
+        height={cardHeight}
       >
         <Image
           src="Owner_Big_Photo.jpg"
           alt="Real Estate"
-          w="100%"
-          h="100%"
+          width="100%"
+          height="100%"
           objectFit="cover"
           borderRadius="md"
         />
@@ -49,8 +47,8 @@ const Unique = () => {
         flex="1"
         direction="column"
         p={8}
-        minH={minHeight}
-        maxH={maxHeight}
+        height={cardHeight}
+        justify="center"
       >
         <Text
           fontSize="12.5px"
@@ -96,3 +94,102 @@ const Unique = () => {
 };
 
 export default Unique;
+
+
+
+
+
+// import React from "react";
+// import {
+//   Text,
+//   Flex,
+//   Image,
+//   Button,
+//   useBreakpointValue,
+// } from "@chakra-ui/react";
+// import { Link } from "react-router-dom";
+
+// const Unique = () => {
+//   const flexDirection = useBreakpointValue({ base: "column", md: "row" });
+//   const minHeight = useBreakpointValue({ base: "400px", md: "500px" });
+
+//   return (
+//     <Flex
+//       direction={flexDirection}
+//       borderWidth={1}
+//       borderRadius="md"
+//       boxShadow="md"
+//       bg="white"
+//       align="center"
+//       overflow="hidden"
+//       minH={minHeight}
+//     >
+//       {/* Left side with image */}
+//       <Flex
+//         flex="1"
+//         justify="center"
+//         align="center"
+//         p={1}
+//         minH={minHeight}
+//       >
+//         <Image
+//           src="Owner_Big_Photo.jpg"
+//           alt="Real Estate"
+//           w="100%"
+//           h="100%"
+//           objectFit="cover"
+//           borderRadius="md"
+//         />
+//       </Flex>
+
+//       {/* Right side with text and button */}
+//       <Flex
+//         flex="1"
+//         direction="column"
+//         p={8}
+//         minH={minHeight}
+//       >
+//         <Text
+//           fontSize="12.5px"
+//           color="#5ea51d"
+//           fontFamily="'Nunito Sans', Arial, sans-serif"
+//           fontWeight="800"
+//         >
+//           Our Special
+//         </Text>
+//         <Text
+//           fontSize={["16px", "24px", "30px", "36px"]}
+//           color="#000000CC"
+//           fontFamily="'Nunito Sans', Arial, sans-serif"
+//           mb={6}
+//         >
+//           What makes us unique from others
+//         </Text>
+//         <Text fontSize="lg" color="gray.600" mb={8}>
+//           <ul style={{ listStyleType: "disc", paddingLeft: "20px" }}>
+//             <li>Exceptional customer service</li>
+//             <li>Unparalleled market expertise</li>
+//             <li>Dedication to finding the perfect property</li>
+//             <li>Experienced professionals staying ahead of market trends</li>
+//             <li>Innovative solutions and personalized service</li>
+//             <li>
+//               Building lasting relationships grounded in trust, integrity, and transparency
+//             </li>
+//           </ul>
+//         </Text>
+//         <Button
+//           as={Link}
+//           to="/about"
+//           bg="#5ea51d"
+//           size="md"
+//           mt="auto"
+//           width="200px"
+//         >
+//           Read more
+//         </Button>
+//       </Flex>
+//     </Flex>
+//   );
+// };
+
+// export default Unique;
