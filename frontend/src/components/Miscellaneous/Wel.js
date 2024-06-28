45f0da9c04db97f2fd1a1ab923dab22a0bf6150b
kolliDayakar77kolli <Kollidayakar77@gmail.com>
2024-06-28 10:07:21 +0530

    dropcap text

--- frontend/src/components/Miscellaneous/Wel.js
+++ frontend/src/components/Miscellaneous/Wel.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import { Box, Flex, Image, Heading,Text } from "@chakra-ui/react";
 
 const Wel = () => {
   return (
     <Flex
-      mt="130px"
+      mt="100px"
       direction={{ base: "column", md: "row" }}
       alignItems="center"
       justifyContent="center"
@@ -21,18 +21,47 @@ const Wel = () => {
         justifyContent="center"
       >
         <Box>
-          <Text fontSize="xl" fontWeight="bold">
+          <Heading
+            as="h1"
+            size="4xl"
+            color="#5ea51d"
+            mb={4}
+            fontFamily="'Nunito Sans', Arial, sans-serif"
+          >
             Welcome to Ratna Real Estate
+          </Heading>
+          <Text
+            fontSize="12.5px"
+            color="#5ea51d"
+            fontFamily="'Nunito Sans', Arial, sans-serif"
+            textAlign="center"
+            fontWeight="800"
+          >
+            Welcome to Ratna Real Estate, where expertise meets exceptional
+            service.
           </Text>
-          <Text mt={2}>
-            Welcome to Ratna Real Estate, where expertise meets
-            exceptional service. I’m Galla Srinivas Rao, the founder and principal
-            broker of Ratna Real Estate. With over 10 years in
-            the real estate industry, I have cultivated a reputation for
-            excellence, integrity, and client-focused service. Our mission is to
-            provide unparalleled real estate solutions tailored to each client’s
-            unique needs and aspirations.
-          </Text>
+
+          <Box>
+            <Text
+              as="span"
+              fontSize="4em"
+              float="left"
+              lineHeight="0.8"
+              mr="0.1em"
+              color="#081637"
+              fontFamily="'Nunito Sans', Arial, sans-serif"
+            >
+              I’
+            </Text>
+            <Text as="span"  mt={2} fontSize="md" color="gray.600" lineHeight="taller">
+              I’m Galla Srinivas Rao, the founder and principal broker of Ratna
+              Real Estate. With over 10 years in the real estate industry, I have
+              cultivated a reputation for excellence, integrity, and
+              client-focused service. Our mission is to provide unparalleled real
+              estate solutions tailored to each client’s unique needs and
+              aspirations.
+            </Text>
+          </Box>
         </Box>
       </Box>
 
