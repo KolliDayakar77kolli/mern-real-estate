934156b48f738460cdeb2618db342d5f42204683
kolliDayakar77kolli <Kollidayakar77@gmail.com>
2024-06-28 10:18:42 +0530

    wrap

--- frontend/src/components/Miscellaneous/Wel.js
+++ frontend/src/components/Miscellaneous/Wel.js
@@ -21,30 +21,29 @@ const Wel = () => {
         justifyContent="center"
       >
         <Box>
+          <Text
+            fontSize="12.5px"
+            color="#5ea51d"
+            fontFamily="'Nunito Sans', Arial, sans-serif"
+            fontWeight="800"
+          >
+            ABOUT US
+          </Text>
+
           <Heading
             as="h1"
-            size="4xl"
-            color="#5ea51d"
+            size="3xl"
+            color="#081637"
             mb={4}
             fontFamily="'Nunito Sans', Arial, sans-serif"
           >
             Welcome to Ratna Real Estate
           </Heading>
-          <Text
-            fontSize="12.5px"
-            color="#5ea51d"
-            fontFamily="'Nunito Sans', Arial, sans-serif"
-            textAlign="center"
-            fontWeight="800"
-          >
-            Welcome to Ratna Real Estate, where expertise meets exceptional
-            service.
-          </Text>
 
           <Box>
             <Text
               as="span"
-              fontSize="4em"
+              fontSize="3em"
               float="left"
               lineHeight="0.8"
               mr="0.1em"
@@ -53,8 +52,8 @@ const Wel = () => {
             >
               I’
             </Text>
-            <Text as="span"  mt={2} fontSize="md" color="gray.600" lineHeight="taller">
-              I’m Galla Srinivas Rao, the founder and principal broker of Ratna
+            <Text as="span"  mt={2} fontSize="md" color="gray.600" lineHeight="taller" textAlign="justify">
+              m Galla Srinivas Rao, the founder and principal broker of Ratna
               Real Estate. With over 10 years in the real estate industry, I have
               cultivated a reputation for excellence, integrity, and
               client-focused service. Our mission is to provide unparalleled real
